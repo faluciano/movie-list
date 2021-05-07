210dc3f5f1b3a8991c7b4e45822ff959e843f624
Felix Luciano <felixluciano.a@gmail.com>
2021-05-07 16:49:18 -0400

    Added material ui components to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Movie from './Movie';
 
+
 function App() {
   return (
     <div className="App">
       <Movie/>
+      
     </div>
   );
 }
--- frontend/src/Movie.js
+++ frontend/src/Movie.js
@@ -1,15 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import {makeStyles} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow:1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 function Movie() {
     const [movies, setMovies] = useState([]);
+    const classes = useStyles();
     function getMovies(){
         axios.get("/movie").then(response=>setMovies(response.data));
     }
     return (
-        <div>
-            <button onClick={getMovies}>Get Movies</button>
-            <h1>Movies are: {movies.map(movie=><li>{movie}</li>)}</h1>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <TextField margin="normal" variant="outlined"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <h1>Movies are: {movies.map(movie=><li>{movie}</li>)}</h1>
+                </Grid>
+                <Grid item xs={6}>
+                    <button onClick={getMovies}>Get Movies</button>
+                </Grid>
+            </Grid>
+            
         </div>
     )
 }
