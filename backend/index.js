c3e443cc091775937938acdd1e77f5dd29416ff5
Felix Luciano <felixluciano.a@gmail.com>
2021-05-08 23:31:22 -0400

    Added production settings

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 const result = require('dotenv').config({path:'../.env'}).parsed;
 const key = "OMDB_KEY" in process.env ?process.env.OMDB_KEY:result.OMDB_KEY;
+app.use(express.static(path.resolve(__dirname, '..client/build')));
 
 function getRating(id){
     return (axios.request({
@@ -33,7 +34,6 @@ app.get("/rating",async (req,res)=>{
     console.log(m.imdbRating);
     res.send(m.imdbRating);
 });
-app.get("/", (req,res) => res.send("Hello World!"));
 app.get("/movie", (req,res)=>{
     let movie = req.query.movie;
     console.log(movie);
@@ -48,6 +48,10 @@ app.get("/movie", (req,res)=>{
         .then((response)=>"Error" in response.data?res.send(""):(compute(response.data)
             .then((val)=>res.send(val))))
         .catch((error)=>res.send(error))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen( port,'0.0.0.0',()=>console.log(`Running app on port localhost ${port}`));
