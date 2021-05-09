69b30539e2fc911185a88c0803252b41a34d9855
Felix Luciano <felixluciano.a@gmail.com>
2021-05-08 23:51:05 -0400

    removed not needed server endpoint

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 const result = require('dotenv').config({path:'../.env'}).parsed;
 const key = "OMDB_KEY" in process.env ?process.env.OMDB_KEY:result.OMDB_KEY;
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname,'..','client','build')));
 
 function getRating(id){
     return (axios.request({
@@ -49,10 +49,6 @@ app.get("/movie", (req,res)=>{
         .then((response)=>"Error" in response.data?res.send(""):(compute(response.data)
             .then((val)=>res.send(val))))
         .catch((error)=>res.send(error))
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen( port,'0.0.0.0',()=>console.log(`Running app on port localhost ${port}`));
